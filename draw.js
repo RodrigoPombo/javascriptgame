69e01fb786360538545c20a6c9917f4e5510a1a3
Rodrigo Pombo <ropombo@microsoft.com>
2020-04-19 23:33:38 +0100

    My FIRST COMMIT!

--- draw.js
+++ draw.js
@@ -1,57 +1,50 @@
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
-//console.log(canvas)
+console.log(canvas)
 const scale = 10;
 const rows = canvas.height /scale;
 const columns = canvas.height /scale;
 var snake;
 var circle;
-var dif = 60;
-var interval = 1000;
+var dif = 20;
+var interval;
+
 
 function setupdif()
 {
-        this.dif = this.dif - 10;
-        console.log("dif atual: " + this.dif);
+    if(player.score%1 == 0 && player.score!=0)
+    {
+        this.dif = 1;
+        console.log("dif autal: " +this.dif);
+    }
 }
 
-var snake = new Snake();
-var circle = new Circle();
-var player = new Player();
 function setup(){
+    snake = new Snake();
+    circle = new Circle();
+    player = new Player();
 
-    //this.dif = this.dif - 1;
+    this.dif = this.dif - 10;
 
-    interval = window.setInterval(()=>{
+    window.setInterval(()=>{
 
+    if ( player.score == 2 ) {
+        setup();
+    } else {
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        this.snake.draw();
-        this.circle.draw();
-        this.snake.update();
-        this.circle.update();
-        console.log("dif atual: " + this.dif);
+        snake.draw();
+        circle.draw();
+        snake.update();
+        circle.update();
 
         if((circle.centerY < (snake.y+ 90) && circle.centerY > snake.y) && (circle.centerX < 0))
         {
             circle.xSpeedCircle = +scale;
             circle.update();
             player.score = player.score + 1;
-            setupdif(player.score)
+            setupdif(player.score);
          }
-
-        window.clearInterval(interval);
-    
-        if ( player.score % 2 == 0 && player.score != 0) 
-        {
-            //setupdif(player.score)
-            setup();
-            //console.log("here");
-        } 
-        else 
-        {
-            setup();
-            //console.log("here");
-        }
+    }
 
     //window.clearInterval(interval); 
 
@@ -60,12 +53,17 @@ function setup(){
     ctx.fillText("your dif atual: " + (50-dif), 740, 100);
 
     },this.dif)
+
 };
 
 setup();
 
 window.addEventListener("keydown",(evt) => {
 const direction = evt.key.replace("Arrow","");
+//console.log(direction);
+
 snake.changeDir(direction);
+
+
 })
 
