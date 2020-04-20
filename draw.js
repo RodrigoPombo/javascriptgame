c062bd8dc10462a697f4dc46b55379dc00a47265
Rodrigo Pombo <ropombo@microsoft.com>
2020-04-20 15:50:12 +0100

    fixed the difficult upgrade

--- draw.js
+++ draw.js
@@ -6,43 +6,60 @@ const rows = canvas.height /scale;
 const columns = canvas.height /scale;
 var snake;
 var circle;
-var dif = 1;
+var dif = 60;
 var interval = 1000;
 
 function setupdif()
 {
-        this.dif = this.dif + 1;
-        //console.log("dif atual: " + this.dif);
+        this.dif = this.dif - 10;
+        console.log("dif atual: " + this.dif);
 }
 
 var snake = new Snake();
-var rand_point = new rand_point();
-//var player = new Player();
+var circle = new Circle();
+var player = new Player();
 function setup(){
+
     //this.dif = this.dif - 1;
 
     interval = window.setInterval(()=>{
 
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        this.snake.draw(this.dif);
+        this.snake.draw();
+        this.circle.draw();
         this.snake.update();
-        this.rand_point.draw(); 
+        this.circle.update();
+        console.log("dif atual: " + this.dif);
 
-        if(((this.snake.x+30) >= this.rand_point.x_rand) && (this.snake.x <= this.rand_point.x_rand) && (((this.snake.y+30) >= this.rand_point.y_rand) && (this.snake.y) <= this.rand_point.y_rand))
-        { 
-            //console.log("!!cheguei aqui!!");
-            this.rand_point.update_rand();
-            snake.size = snake.size + 1;
-            //this.rand_point.draw(snake.size);
-            //console.log("tamanho da cobra: " + snake.size);
-            setupdif();
+        if((circle.centerY < (snake.y+ 90) && circle.centerY > snake.y) && (circle.centerX < 0))
+        {
+            circle.xSpeedCircle = +scale;
+            circle.update();
+            player.score = player.score + 1;
+            setupdif(player.score)
+         }
+
+        window.clearInterval(interval);
+    
+        if ( player.score % 2 == 0 && player.score != 0) 
+        {
+            //setupdif(player.score)
+            setup();
+            //console.log("here");
+        } 
+        else 
+        {
+            setup();
+            //console.log("here");
         }
 
-    //ctx.font = "30px Arial";
-    //ctx.fillText("your score atual: " + player.score, 740, 50);
-    //ctx.fillText("your dif atual: " + (50-dif), 740, 100);
+    //window.clearInterval(interval); 
 
-    },90)
+    ctx.font = "30px Arial";
+    ctx.fillText("your score atual: " + player.score, 740, 50);
+    ctx.fillText("your dif atual: " + (50-dif), 740, 100);
+
+    },this.dif)
 };
 
 setup();
