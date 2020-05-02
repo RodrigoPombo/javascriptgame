35ef9a991395f4a1ea4cd721743ceb642edb8c21
Rodrigo Pombo <ropombo@microsoft.com>
2020-05-03 00:25:37 +0100

    First Snake Game Commit. Self-learning for JS.

--- draw.js
+++ draw.js
@@ -1,75 +1,54 @@
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
-console.log(canvas)
+//console.log(canvas)
 const scale = 10;
 const rows = canvas.height /scale;
 const columns = canvas.height /scale;
 var snake;
 var circle;
-var dif = 60;
+var dif = 1;
 var interval = 1000;
 
 function setupdif()
 {
-        this.dif = this.dif - 10;
-        console.log("dif atual: " + this.dif);
+        this.dif = this.dif + 1;
+        //console.log("dif atual: " + this.dif);
 }
 
 var snake = new Snake();
-var circle = new Circle();
-var player = new Player();
+var rand_point = new rand_point();
+//var player = new Player();
 function setup(){
-
     //this.dif = this.dif - 1;
 
     interval = window.setInterval(()=>{
 
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        this.snake.draw();
-        this.circle.draw();
+        this.snake.draw(this.dif);
         this.snake.update();
-        this.circle.update();
-        console.log("dif atual: " + this.dif);
+        this.rand_point.draw(); 
 
-        if((circle.centerY < (snake.y+ 90) && circle.centerY > snake.y) && (circle.centerX < 0))
-        {
-            circle.xSpeedCircle = +scale;
-            circle.update();
-            player.score = player.score + 1;
-            setupdif(player.score)
-         }
-
-        window.clearInterval(interval);
-    
-        if ( player.score % 2 == 0 && player.score != 0) 
-        {
-            //setupdif(player.score)
-            setup();
-            //console.log("here");
-        } 
-        else 
-        {
-            setup();
-            //console.log("here");
+        if(((this.snake.x+30) >= this.rand_point.x_rand) && (this.snake.x <= this.rand_point.x_rand) && (((this.snake.y+30) >= this.rand_point.y_rand) && (this.snake.y) <= this.rand_point.y_rand))
+        { 
+            //console.log("!!cheguei aqui!!");
+            this.rand_point.update_rand();
+            snake.size = snake.size + 1;
+            //this.rand_point.draw(snake.size);
+            //console.log("tamanho da cobra: " + snake.size);
+            setupdif();
         }
 
-    //window.clearInterval(interval); 
+    //ctx.font = "30px Arial";
+    //ctx.fillText("your score atual: " + player.score, 740, 50);
+    //ctx.fillText("your dif atual: " + (50-dif), 740, 100);
 
-    ctx.font = "30px Arial";
-    ctx.fillText("your score atual: " + player.score, 740, 50);
-    ctx.fillText("your dif atual: " + (50-dif), 740, 100);
-
-    },this.dif)
+    },90)
 };
 
 setup();
 
 window.addEventListener("keydown",(evt) => {
 const direction = evt.key.replace("Arrow","");
-//console.log(direction);
-
 snake.changeDir(direction);
-
-
 })
 
--- snake.js
+++ snake.js
@@ -9,11 +9,164 @@ function Snake()
     this.y = 0;
     this.xSpeed = 0;
     this.ySpeed = 0; 
+    this.size = 1;
+    this.orient = "Right";
+    this.orient_prev="Right";
+    this.list_letter = [];
+    this.list_letter = [];
+    this.state = "";
 
-    this.draw =  function()
+    this.draw =  function(dif)
     {
-        ctx.fillStyle = "#FFFFFF";
-        ctx.fillRect(this.x, this.y, 30,90);
+        //console.log("!!entrou no draw!!");
+        this.ori = this.orient_prev;
+        if(this.orient != this.orient_prev)
+        {
+            this.orient_prev = this.orient;
+            this.list_letter.unshift(this.orient);
+            //////console.log("mudou!: acrescentou " + this.list_letter[0])
+        }
+        else
+        {
+            this.list_letter.unshift("dummy");
+            //////console.log("acrescentou dummy " + this.list_letter[0])
+        }
+        
+        var x_now = this.x;
+        var y_now = this.y;
+
+        for(i = 0 ; i < dif ; i++)
+            {
+                //console.log("##########################" + i);
+                ////console.log("x_now: " + x_now  );
+                ////console.log("y_now: " + y_now);
+                
+                for(j = 0 ; j < dif ; j++)
+                {
+                    //console.log("elemento: " + j + " da array: " + this.list_letter[j]);
+                }
+
+                if(i == 0)
+                {
+                    ctx.fillStyle = "#FFFFFF";
+                    ctx.fillRect(this.x, this.y, 30,30);
+                    state = this.orient;
+                    ////console.log("coordenada do primeiro da lst: " + this.list_letter[i]);
+                    //////console.log("coordenada de x: " + this.x);
+                    //////console.log("coordenada de y: " + this.y);
+                }
+                else
+                {
+                    //tenho de alterar isto!!
+                    if(this.list_letter[i] == "dummy")
+                    {
+                        if(i == 2)
+                        {
+                            if(this.list_letter[0] != "dummy")
+                            {
+                            state = this.ori;
+                            console.log("!!!!!!!!!!!!!!entrei na condição do 2º!!!!!!!!!!!!!!!!!! + previ state: "  + this.state);
+                            }
+                        }
+
+                        if(state == "Right")
+                        {
+                            x_now = (x_now - 30);
+                            //y_now = y_now;
+                            ctx.fillStyle = "#FFFFFF";
+                            ctx.fillRect(x_now, y_now, 30,30);
+                            //////console.log("array: " + this.list_letter[i]);
+                            //break;;
+                        }
+                        if(state == "Left")
+                        {
+                            x_now = (x_now + 30);
+                            //y_now = y_now;
+                            ctx.fillStyle = "#FFFFFF";
+                            ctx.fillRect(x_now, y_now, 30,30);
+                            //////console.log("array: " + this.list_letter[i]);
+                            //break;;
+                        } 
+                        if(state == "Up")
+                        {
+                            //x_now = x_now + 30;
+                            y_now = (y_now + 30);
+                            ctx.fillStyle = "#FFFFFF";
+                            ctx.fillRect(x_now, y_now, 30,30);
+                            ////console.log("entrou no up caixote nº " + (i+1));
+                            ////console.log("coordenada de x: " + this.x_now);
+                            ////console.log("coordenada de y: " + this.y_now);
+                            //break;;
+                        } 
+                        if(state == "Down")
+                        {
+                            //x_now = x_now + 30;
+                            y_now = (y_now - 30);
+                            ctx.fillStyle = "#FFFFFF";
+                            ctx.fillRect(x_now, y_now, 30,30);
+                            //////console.log("array: " + this.list_letter[i]);
+                            //break;
+                        } 
+                        //console.log("posição: " + i + " entrou no dummy");
+                        //console.log("x_now: " + x_now + " y_now: " + y_now);
+
+                        if(i == 1 || i == 2)
+                        {
+                            console.log("para a posição " + i);
+                            console.log("x: " + x_now + " y: " + y_now);
+                        }
+                    }
+                    else
+                    {
+                        state = this.list_letter[i];
+
+                        if(state == "Right")
+                        {
+                            x_now = (x_now - 30);
+                            //y_now = y_now;
+                            ctx.fillStyle = "#FFFFFF";
+                            ctx.fillRect(x_now, y_now, 30,30);
+                            //break;;
+                        }
+                        if(state == "Left")
+                        {
+                            x_now = (x_now + 30);
+                            //y_now = y_now;
+                            ctx.fillStyle = "#FFFFFF";
+                            ctx.fillRect(x_now, y_now, 30,30);
+                            //break;;
+                        } 
+                        if(state == "Up")
+                        {
+                            //x_now = x_now + 30;
+                            y_now = (y_now + 30);
+                            //console.log("entrou no UP do n dummy: com valor y: " + this.y);
+                            //console.log("entrou no UP do n dummy: com valor y_now: " + y_now);
+                            ctx.fillStyle = "#FFFFFF";
+                            ctx.fillRect(x_now, y_now, 30,30);
+                            ////console.log("y_now final: " + y_now);
+                            ////console.log("x_now final: " + x_now);
+                            //break;;
+                        } 
+                        if(state == "Down")
+                        {
+                            //x_now = x_now + 30;
+                            y_now = (y_now - 30);
+                            ctx.fillStyle = "#FFFFFF";
+                            ctx.fillRect(x_now, y_now, 30,30);
+                            //////console.log("array: " + this.list_letter[i]);
+                            //break;;
+                        } 
+                        //console.log("posição: " + i + " entrou no não dummy");
+                        //console.log("x_now: " + x_now + " y_now: " + y_now);
+
+
+                        
+                    }
+                }
+            
+            
+            }
     }
 
     this.update = function()
@@ -25,16 +178,28 @@ function Snake()
 
         if(this.y > 900)
         {
-            this.ySpeed = -scale;
+            alert("This alert will never be shown.");
         }
         
-        if(this.y < 0)
+        if(this.y < -10)
         {
-            this.ySpeed = scale;
+            alert("This alert will never be shown.");
         }
 
-        //console.log(this.y);
+        if(this.x > 1000)
+        {
+            alert("This alert will never be shown.");
+        }
+        
+        if(this.x < -10)
+        {
+            alert("This alert will never be shown.");
+        }
+
+        //////console.log(this.y);
     }
+
+
 
     this.changeDir = function(direct)
     {
@@ -42,60 +207,56 @@ function Snake()
             case "Up":
                 this.xSpeed = 0;
                 this.ySpeed = -scale;
+                this.orient="Up";
+                //this.list.unshift("U");
+                //this.list.unshift("U");
                 break;
-                //console.log(this.ySpeed);
             case "Down":
                 this.xSpeed = 0;
                 this.ySpeed = scale;
+                this.orient="Down";
+                //this.list.unshift("D");
+                //this.list.unshift("U");
                 break;
+            case "Right":
+                this.xSpeed = scale;
+                this.ySpeed = 0;
+                this.orient = "Right";
+                //this.list.unshift("R");
+                //this.list.unshift("U");
+                break;
+            case "Left":
+                this.xSpeed = -scale;
+                this.ySpeed = 0;
+                this.orient = "Left";
+                //this.list.unshift("L");
+                //this.list.unshift("U");
+                break;
+
         }
     }
 }
 
 
-function Circle() 
+function rand_point() 
 {
-    this.centerX = 65;
-    this.centerY = 50;
-    this.radius = 30;
-    this.toX = 10;
-    this.toY = 0;
-    this.xSpeedCircle = 30;
-    this.ySpeedCircle = 3;
+    this.x_rand = Math.floor(Math.random() * 1000);  
+    this.y_rand = Math.floor(Math.random() * 900);  
+    
+    //this.x_rand = Math.floor(Math.random() * 50);  
+    //this.y_rand = Math.floor(Math.random() * 50);  
 
+    this.update_rand =  function()
+    {
+        this.x_rand = Math.floor(Math.random() * 1000);  
+        this.y_rand = Math.floor(Math.random() * 900);  
+      //  this.x_rand = Math.floor(Math.random() * 50);  
+       // this.y_rand = Math.floor(Math.random() * 50); 
+    }
+    
     this.draw =  function()
     {
         ctx.fillStyle = "#FFFFFF";
-        ctx.fillRect(this.centerX ,this.centerY , 30,30);
+        ctx.fillRect(this.x_rand, this.y_rand, 10,10);
     }
-
-    this.update = function()
-    {
-        this.centerX = this.centerX + this.xSpeedCircle;
-        this.centerY = this.centerY + this.ySpeedCircle;
-
-        if(this.centerX > 1000)
-        {
-            this.xSpeedCircle = -scale;
-        }
-        
-        if(this.centerX < -50)
-        {
-            ctx.font = "30px Arial";
-            alert("Click OK to play again");
-        }
-
-        if(this.centerY > 990)
-        {
-            this.ySpeedCircle = -scale;
-        }
-        
-        if(this.centerY < 0)
-        {
-            this.ySpeedCircle = scale;
-        }
-
-        //console.log(this.ySpeedCircle);
-    }
-
 }
